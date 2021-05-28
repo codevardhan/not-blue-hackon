05045711e83af2e83e069739901b33ff68d13025
sreeragkvivek <sreeragkvivek23@gmail.com>
2021-05-28 22:08:27 +0530

    finished html js

--- extension/popup.js
+++ extension/popup.js
@@ -2,19 +2,24 @@ var x=1
 function appendRow()
 {  
    var d = document.createElement('div');
+   if (x<=3){
    d.innerHTML += "<input type='text' id='tst"+ x++ +"'><br >";
     document.getElementById('additional').appendChild(d);
+   }
+
     
 }
 
-//let flag=document.getElementById("additional")
-//if (flag){
-//  console.log("yayy");
-//}
-
-
+let user
+document.addEventListener('DOMContentLoaded',function (){
+document.getElementById("add").addEventListener("click", function() {
+  console.log("Hello World");
+  });
+});
 
 document.addEventListener('submit', function(e) { 
-e.preventDefault()   
+  e.preventDefault();
+  user = document.getElementById("name").value;
+  console.log(user);  
   appendRow();
 })
