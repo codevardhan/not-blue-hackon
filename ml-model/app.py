2ca243f0c223ca4e2b5bd8390da6eb5506e09d84
codevardhan <harsha7vardhan@gmail.com>
2021-05-28 17:58:48 +0000

    added sending emails functionality

--- ml-model/app.py
+++ ml-model/app.py
@@ -1,12 +1,19 @@
-import pickle, json
-from flask import Flask, jsonify, request
+import pickle, json, smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from flask_restful import Resource, Api
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 api=Api(app)
+auth=HTTPBasicAuth()
+
+USER_DATA= {
+    "admin": "harshisasexybeast"
+}
 
 POSITIVE = False
 NEGATIVE = True
@@ -34,15 +41,39 @@ def predict(text, def_model):
 	return {"label": label, "score": float(score)}
 
 
-class Test_index(Resource):
+class Predict_dep_ml(Resource):
     def post(self):
         model = load_model('models/best_model.h5')
-        data=request.get_json()
+        recipients=request.get_json()
         result = predict(data['search-term'], model)
+        server=smtplib.SMTP_SSL("smtp.gmail.com",465)
+        server.login("notblueorg@gmail.com","HarshSasiAdiAnji101")
+        msg = MIMEMultipart()
+        sender = 'notblueorg@gmail.com'
+        msg['Subject'] = "Friend Alert"
+        msg['From'] = sender
+        msg['To'] = ", ".join(recipients)
+        body = 'This is the body of the email.'
+        body = MIMEText(body)
+        msg.attach(body)
+        server.sendmail(sender, recipients, msg.as_string())
+        server.quit
         return jsonify(result)
-	
+
+class Send_email(Resource):
+    @auth.login_required
+    def post(self):
+        data=request.get_json()
+
 @app.route('/')
 def home():
-	return "API home"
+    return 'API is active! Please send a POST request to "/predict" with the phrase you would like to test. Syntax: {"search-term": "&#60;insert phrase to test&#62;"}'
 
-api.add_resource(Test_index, "/predict")
+@auth.verify_password
+def verify(username, password):
+    if not (username and password):
+        return False
+    return USER_DATA.get(username) == password
+
+api.add_resource(Send_email, "/email")
+api.add_resource(Predict_dep_ml, "/predict")
